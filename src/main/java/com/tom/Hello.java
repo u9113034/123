32023a845443a28f1d26e079d414f7807f059f97
CKM <CKM>
2019-02-17 00:54:05 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -5,5 +5,16 @@ public class Hello {
 //        System.out.println("Hello world");
         Person p = new Person();
         p.hello();
+        int age = 19;   // short & long
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;    //只要看到有小數點的數字JAVA會自動判定為double
+        Float weight2 = 77.5f;
+        double height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Tom";
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -3,12 +3,17 @@ package com.tom
 fun main(args: Array<String>) {
 //    println("Hello kotlin")
 //    Human().hello()
-    val h = Human()
-    h.hello()
-}
+        val h = Human()
+        h.hello()
+        var age : Int = 19
+        age =20
+        var weight = 66.5f
+        var name : String
+        name = "Hank"
+    }
 
-class Human {
-    fun hello() {
+    class Human {
+        fun hello() {
         println("Hello kotlin")
     }
 }
